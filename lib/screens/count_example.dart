c577261055993709519eb98c800b0dc0101552ec
Haroon <rajaharoon973@gmail.com>
2024-08-08 20:51:15 +0500

    part 5

---
+++ lib/screens/count_example.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_provider/provider/count_provider.dart';
+import 'package:provider/provider.dart';
+
+class CountExample extends StatefulWidget {
+  const CountExample({super.key});
+
+  @override
+  State<CountExample> createState() => _CountExampleState();
+}
+
+class _CountExampleState extends State<CountExample> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    final countProvider = Provider.of<CountProvider>(context, listen: false);
+    Timer.periodic(Duration(seconds: 0), (timer) {
+      countProvider.setCount();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final countProvider = Provider.of<CountProvider>(context, listen: false);
+    print('build');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Provider'),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+          child: Consumer<CountProvider>(builder: (context, value, child) {
+        return Text(
+          value.count.toString(),
+          style: const TextStyle(fontSize: 50),
+        );
+      })),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          countProvider.setCount();
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
